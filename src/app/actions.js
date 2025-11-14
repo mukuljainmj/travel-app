cdbee3befbbb4ffba1e2a03bed6894fb9f114e5f
mukuljainmj <41673960+mukuljainmj@users.noreply.github.com>
2025-11-14 09:51:17 +0530

    UI streaming

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,32 +1,13 @@
 "user server";
 
 import { openai } from "@ai-sdk/openai";
-import { z } from "zod";
-import { streamObject } from "ai";
-import { createStreamableValue } from "ai/rsc";
+import { streamUI } from "ai/rsc";
 
-export async function generate(input) {
-  "use server";
-  const stream = createStreamableValue();
-  (async () => {
-    const { partialObjectStream } = await streamObject({
-      model: openai("gpt-4o"), // "gpt-3.5-turbo"
-      system: "You generate fake data for thre people",
-      prompt: input,
-      schema: z.object({
-        people: z.array(
-          z.object({
-            name: z.string().describe("name of a fake person"),
-            address: z.string().describe("US address format"),
-            age: z.number(),
-          })
-        ),
-      }),
-    });
-    for await (const delta of partialObjectStream) {
-      stream.update(delta);
-    }
-    stream.done();
-  })();
-  return { object: stream.value };
+export async function streamUIComponent() {
+  const result = await streamUI({
+    model: openai("gpt-4o"), // "gpt-3.5-turbo"
+    prompt: "Give me some advise to plan a trip to Tolluride, Colorado.",
+    text: ({ content }) => <div>{content}</div>,
+  });
+  return result.value;
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,25 @@
 "use client";
 
 import { useState } from "react";
-import { generate } from "./actions";
-import { getStreamableValue } from "ai/rsc";
+import { streamUIComponent } from "./actions";
 
 export const dynamic = "force-dynamic";
 export const maxDuration = 30;
 
 export default function Home() {
-  const [generation, setGeneration] = useState("");
+  const [component, setComponent] = useState("");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button
-        onClick={async () => {
-          const { object } = await generate(
-            "People who sound like they have superhero names"
-          );
-          for await (const delta of getStreamableValue(object)) {
-            if (delta) {
-              setGeneration(JSON.stringify(delta.people, null, 2));
-            }
-          }
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const comp = await streamUIComponent();
+          setComponent(comp);
         }}
       >
-        View People!
-      </button>
-      <pre>{generation}</pre>
+        <button type="submit">Get Component!</button>
+      </form>
+      <div>{component}</div>
     </main>
   );
 }
